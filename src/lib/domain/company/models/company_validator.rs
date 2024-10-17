16d4dc38ec95cad35c376c867c9371f9f7b5306d
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-17 14:09:13 +0200

    chore: Add CreateCompany and AttachProfessionalInCompany models
    The code changes introduce the `CreateCompany` and `AttachProfessionalInCompany` models in the `company_validator.rs` file. These models are used for creating a new company and attaching a professional to a company, respectively.
    
    Note: The recent user commits and repository commits are not relevant for generating the commit message.
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/lib/domain/company/models/company_validator.rs
+++ src/lib/domain/company/models/company_validator.rs
@@ -1,11 +1,20 @@
-#[derive(Debug, Clone)]
+use serde::Deserialize;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Deserialize)]
 pub struct CreateCompany {
-  pub name: String,
-  pub city: String,
-  pub country: String,
-  pub email: String,
-  pub phone: String,
-  pub zip_code: String,
-  pub address: String,
-  pub national_code: String,
+    pub name: String,
+    pub city: String,
+    pub country: String,
+    pub email: String,
+    pub phone: String,
+    pub zip_code: String,
+    pub address: String,
+    pub national_code: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Deserialize)]
+pub struct AttachProfessionalInCompany {
+    pub professional_id: String,
+    pub role: String,
+    pub permissions: Vec<String>,
 }
