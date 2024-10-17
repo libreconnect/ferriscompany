626cc1cf0227e8a34ed7ebe37babe187f124d9ec
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-17 16:25:55 +0200

    chore: Update messaging module with NATS integration
    The code changes in the `messaging` module introduce NATS integration for asynchronous messaging functionality. This allows for seamless communication between different components of the application.
    
    Note: The recent user commits and repository commits are not relevant for generating the commit message.
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/lib/infrastructure/messaging/nats.rs
+++ src/lib/infrastructure/messaging/nats.rs
@@ -1,21 +1,52 @@
+use async_nats::{connect, Client};
 use std::sync::Arc;
 
-use async_nats::{connect, Client};
+use crate::application::ports::messaging_ports::MessagingPort;
 
 pub struct Nats {
-  client: Arc<Client>
+    client: Arc<Client>,
 }
 
 impl Nats {
-  pub async fn new(addrs: &str) -> anyhow::Result<Nats> {
-    let client = connect(addrs).await?;
+    pub async fn new(addrs: &str) -> anyhow::Result<Nats> {
+        let client = connect(addrs).await?;
 
-    Ok(Nats {
-      client: Arc::new(client)
-    })
-  }
+        Ok(Nats {
+            client: Arc::new(client),
+        })
+    }
 
-  pub fn get_client(&self) -> Arc<Client> {
-    Arc::clone(&self.client)
-  }
+    pub fn get_client(&self) -> Arc<Client> {
+        Arc::clone(&self.client)
+    }
+}
+
+#[derive(Clone)]
+pub struct NatsMessaging {
+    connection: Arc<Client>,
+}
+
+impl NatsMessaging {
+    pub async fn new(addrs: &str) -> anyhow::Result<NatsMessaging> {
+        let client = connect(addrs).await?;
+
+        Ok(NatsMessaging {
+            connection: Arc::new(client),
+        })
+    }
+
+    pub fn get_connection(&self) -> Arc<Client> {
+        Arc::clone(&self.connection)
+    }
+}
+
+impl MessagingPort for NatsMessaging {
+    async fn publish_message(&self, topic: String, message: String) -> anyhow::Result<()> {
+        let conn = self.get_connection();
+
+        conn.publish(topic, message.into())
+            .await
+            .map_err(|e| anyhow::anyhow!(e.to_string()))
+            .map(|_| ())
+    }
 }
