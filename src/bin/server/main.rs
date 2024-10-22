b9a69b7a576cb0b29cca817d8274f4760540cd97
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-22 09:30:11 +0200

    refactor: Update server main.rs
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -2,8 +2,17 @@ use std::sync::Arc;
 
 use clap::Parser;
 use ferriscompany::{
-    application::http::{HttpServer, HttpServerConfig},
-    domain::company::services::CompanyServiceImpl,
+    application::{
+        http::{HttpServer, HttpServerConfig},
+        ports::{
+            messaging_ports::MessagingPort, messaging_subscriber_port::MessagingSubscriberPort,
+        },
+    },
+    domain::company::{
+        models::message::MessageProfessionalValidated,
+        ports::{CompanyRepository, CompanyService},
+        services::CompanyServiceImpl,
+    },
     env::Env,
     infrastructure::{
         company::neo4j::company_repository::Neo4jCompanyRepository,
@@ -29,13 +38,49 @@ async fn main() -> anyhow::Result<()> {
 
     let messaging = Arc::new(NatsMessaging::new(&env.nats_url).await?);
 
-
     let server_config = HttpServerConfig::new(env.port.clone());
 
     let company_repository = Neo4jCompanyRepository::new(Arc::clone(&neo4j));
-    let company_service = CompanyServiceImpl::new(company_repository, Arc::clone(&messaging));
+    let company_service = Arc::new(CompanyServiceImpl::new(
+        company_repository,
+        Arc::clone(&messaging),
+    ));
 
-    let http_server = HttpServer::new(server_config, Arc::new(company_service)).await?;
+    start_subscriptions(Arc::clone(&company_service), Arc::clone(&messaging)).await;
+
+    let http_server = HttpServer::new(server_config, Arc::clone(&company_service)).await?;
 
     http_server.run().await
+}
+
+async fn start_subscriptions<C, M>(
+    company_service: Arc<CompanyServiceImpl<C, M>>,
+    messaging: Arc<NatsMessaging>,
+) where
+    C: CompanyRepository,
+    M: MessagingPort,
+{
+    let company_service = Arc::clone(&company_service);
+    let messaging = Arc::clone(&messaging);
+
+    tokio::spawn(async move {
+        let result = messaging
+            .subscribe(
+                "professional.add.validated",
+                move |e: MessageProfessionalValidated| {
+                    let t = Arc::clone(&company_service);
+
+                    async move {
+                        let company_service = Arc::clone(&t);
+                        let _ = company_service.handle_professional_validated(e).await;
+                        Ok(())
+                    }
+                },
+            )
+            .await;
+
+        if let Err(e) = result {
+            tracing::error!("Error subscribing to topic: {:?}", e);
+        }
+    });
 }
