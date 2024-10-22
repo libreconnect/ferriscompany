e70c8f0365e304422d26e18e4b787e12ecda52c4
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-22 09:28:57 +0200

    feat: Add messaging subscriber port module
    This commit adds a new module `messaging_subscriber_port` to the `application/ports` directory. The `messaging_subscriber_port` module defines the `MessagingSubscriberPort` trait, which provides a contract for subscribing to messages on a specific topic and handling them asynchronously. This module is necessary for implementing asynchronous messaging functionality in the application.
    
    Note: The recent user commits and repository commits are not relevant for generating the commit message.
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

---
+++ src/lib/application/ports/messaging_subscriber_port.rs
@@ -0,0 +1,15 @@
+use std::{fmt::Debug, future::Future};
+
+use serde::de::DeserializeOwned;
+
+pub trait MessagingSubscriberPort: Clone + Send + Sync + 'static {
+    fn subscribe<F, T, Fut>(
+        &self,
+        topic: &str,
+        handler: F,
+    ) -> impl Future<Output = anyhow::Result<()>> + Send
+    where
+        F: Fn(T) -> Fut + Send + Sync + 'static,
+        Fut: Future<Output = anyhow::Result<()>> + Send + 'static,
+        T: DeserializeOwned + Send + Sync + Debug + 'static;
+}
